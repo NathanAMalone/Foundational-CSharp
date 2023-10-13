4dedb2951e8650e8c6be945c529ae09502e5e494
Nathan Malone <nathanamalone@gmail.com>
2023-10-12 19:29:18 -0500

    overloaded methods

---
+++ OverloadedMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿Random dice = new Random();
+// No Limit; value from 0 to 2,147,483,647 (max int value)
+int roll1 = dice.Next();
+
+// Sets Max value at < 101
+int roll2 = dice.Next(101);
+
+// set value beteween 50 < 101
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
---
+++ testProject1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, C#");
+
+// Random is a stateful method.  It relies on values stored in memory.
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+//Below will not work because you are trying to call Random like a stateless method.
+// Must create a new instance of its class first due to being stateful.
+    // int result = Random.Next();
