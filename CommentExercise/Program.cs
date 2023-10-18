ced9737b204285ad145548e27b7633f1b7cccfd5
Nathan Malone <nathanamalone@gmail.com>
2023-10-17 19:04:35 -0500

    Looking at comments and what makes them useful

---
+++ CommentExercise/Program.cs
@@ -0,0 +1,75 @@
+﻿// Microsoft Training: Readable Code:  Unit 3 Exercise, Create effective code comments
+
+/*Code comments used to document potential change and temp disable old as you test new
+
+string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+
+If new code works, delete old code/comments; should look like next comment block if 
+    it was not commented out
+*/
+
+/*Replacing existing code with new code and comments
+
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+
+*/
+
+/*These comments unnecessarily explain obvious functionality of individual lines of code
+    Considered low-quality comments because they merely explain how C# works.
+    Code also doesn't provide context to problem being solved.
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+Remove existing comments*/
+
+
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+// Comment's usefulness is subjective; have to use best judgement.
